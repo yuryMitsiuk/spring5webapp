efd99aa28bc0294d65132a6c60c6f2a1d1fa4c2c
yuryMitsiuk <yriymitsuik@gmail.com>
2018-06-27 13:08:16 +0300

    adding controller & view for author

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -15,7 +15,7 @@ public class BookController {
     }
 
     @RequestMapping("/books")
-    public String getBook(Model model) {
+    public String getBooks(Model model) {
         model.addAttribute("books", bookRepository.findAll());
         return "books";
     }
